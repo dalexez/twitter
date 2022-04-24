5520b2c97fbc6423592d5c1e91b7badac61c1a21
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-24 15:59:14 -0500

    Requerimiento 3 y su prueba PASS

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -4,7 +4,7 @@ class UserView {
     static createUser(payload){
         if (payload == null) {
             return {error: "payload no existe"}
-        }else if(typeof payload.username !== 'string' || typeof payload.name !== 'string' || typeof payload.id !== 'number'){
+        }else if((typeof payload.username !== 'string' || payload.username == null) || (typeof payload.name !== 'string' || payload.name == null) || (typeof payload.id !== 'number' || payload.id == null)){
             return {error: "necesitan tener un valor valido"}
         }
         return {error: "Todo bien"}
--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -12,4 +12,9 @@ describe("Tests fo UserView", () => {
 
         expect(result.error).toMatch(/necesitan tener un valor valido/)
     })
+    test("Return an error object when try to create a new user with a payload with missing properties", () => {
+        const payload = {username: "Username"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+    })
 })
