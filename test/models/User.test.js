8f5c6647777ca5771c342e23ed9cecf276c44fe1
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-24 02:18:01 -0500

    Requerimiento 3 y su prueba

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -4,15 +4,22 @@ describe("Unit Tests for User class", () => {
 
     test('Create an User object', () =>{
         //Aqui invocas el codigo que vas a usar en tu app
-        const user = new User(1, "carlogilmar", "Carlo", "Bio")
+        const user = new User(1, "dalexez", "Daniel", "Bio")
 
         //Aqui validas los resultados de ese codigo
         //Esta es una comparacion que va a igualar el valor de la izquierda con el de la derecha
         expect(user.id).toBe(1)
-        expect(user.username).toBe("carlogilmar")
-        expect(user.name).toBe("Carlo")
+        expect(user.username).toBe("dalexez")
+        expect(user.name).toBe("Daniel")
         expect(user.bio).toBe("Bio")
         expect(user.dateCreated).not.toBeUndefined()
         expect(user.lastUpdated).not.toBeUndefined() //Verifica que el valor no sea undefined)
+    })
+    test('Add getters', () => {
+        const user = new User(1, "dalexez", "Daniel", "Bio")
+        expect(user.getUsername).toBe("dalexez")
+        expect(user.getBio).toBe("Bio")
+        expect(user.getDateCreated).not.toBeUndefined()
+        expect(user.getLastUpdated).not.toBeUndefined() 
     })
 })
