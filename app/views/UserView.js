7bd7cd18b3843df7ccac62c1ad900ea23021b1d7
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-24 15:48:13 -0500

    Requerimiento 2 y su prueba unitaria

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -4,7 +4,10 @@ class UserView {
     static createUser(payload){
         if (payload == null) {
             return {error: "payload no existe"}
+        }else if(typeof payload.username !== 'string' || typeof payload.name !== 'string' || typeof payload.id !== 'number'){
+            return {error: "necesitan tener un valor valido"}
         }
+        return {error: "Todo bien"}
     }
 }
 
